6c85a0c70c7f239f7f21f55a39aca3f72489f87c
Matijas Levang <micolevang@gmail.com>
2026-04-22 22:12:05 +0200

    [ADD] Get images functionality

--- backend/image-service/src/main.rs
+++ backend/image-service/src/main.rs
@@ -1,11 +1,14 @@
 use axum::{
     extract::{Multipart, Path},
     routing::post,
+    routing::get,
     Router,
     http::StatusCode,
 };
 use std::{fs, io::Write};
 use tokio::net::TcpListener;
+use axum::Json;
+use tower_http::services::ServeDir;
 
 async fn upload_images(
     Path(game_id): Path<i32>,
@@ -33,10 +36,33 @@ async fn upload_images(
     Ok(StatusCode::OK)
 }
 
+async fn get_images(Path(game_id): Path<i32>) -> Json<Vec<String>> {
+    let dir = format!("/app/uploads/{}", game_id);
+
+    let mut urls = vec![];
+    println!("hey we are here");
+    if let Ok(entries) = fs::read_dir(dir) {
+        for entry in entries.flatten() {
+            if let Some(file_name) = entry.file_name().to_str() {
+                let url = format!(
+                    "http://localhost:8003/uploads/{}/{}",
+                    game_id,
+                    file_name
+                );
+                urls.push(url);
+            }
+        }
+    }
+
+    Json(urls)
+}
+
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/images/:game_id", post(upload_images));
+        .route("/images/:game_id", post(upload_images))
+        .route("/images/:game_id", get(get_images))
+        .nest_service("/uploads", ServeDir::new("/app/uploads"));
 
     let listener = TcpListener::bind("0.0.0.0:8003").await.unwrap();
 
