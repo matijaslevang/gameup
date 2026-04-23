c74e79939709e59922b9f8ccc17b9cb813581095
Matijas Levang <micolevang@gmail.com>
2026-04-23 19:26:40 +0200

    [REMOVE] Console logs

--- frontend/src/app/services/image.service.ts
+++ frontend/src/app/services/image.service.ts
@@ -11,12 +11,9 @@ export class ImageService {
   constructor(private http: HttpClient) { }
 
   uploadImagesForGame(gameId: string, files: File[]) {
-    console.log(files)
     const formData = new FormData();
     files.forEach(file => formData.append('files', file, file.name));
-    console.log('FormData keys:', Array.from(formData.keys()));
     for (let entry of formData.entries()) {
-      console.log(entry[0], entry[1]);
     }
     return this.http.post(this.apiUrl + "/" + gameId, formData);
   }
